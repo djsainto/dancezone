c58cf5a1beadd6516c224b8eaffcc5147bba9fd9
Bd591 <bd591@georgetown.edu>
2022-12-09 20:41:21 -0500

    Test Code

--- test/dancezone_test.py
+++ test/dancezone_test.py
@@ -1,6 +1,11 @@
 
-from sourcetest import dancezone
+from sourcetest import recommendation
 
-def test_correct_output():
-    assert dancezone(search_song="Bound 2", track_number="1") == "This song is not a great choice for a party. A more upbeat, danceable song would be better."
-    assert dancezone(search_song="Happy", track_number="4") == "This song is would be good to play for a party!"
+def test_topcase():
+    assert recommendation(danceability=float(75), valence=float(50)) == "This song is would be good to play for a party!"
+
+def test_middlecase():
+    assert recommendation(danceability=float(75), valence=float(20)) == "This song is would be ok to play at a party, but other options might be better."
+
+def test_bottomcase():
+    assert recommendation(danceability=float(20), valence=float(20)) == "This song is not a great choice for a party. A more upbeat, danceable song would be better."
